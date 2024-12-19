66dcc2270c79b86f9050c6fcb2dbd981730cf12c
Jesse205 <2055675594@qq.com>
2024-12-20 07:01:46 +0800

    refactor(vcf_generator): 优化资源路径获取方法
    - 引入 _APP_MODULE_NAME 常量，用于统一模块名称
    - 更新 get_path_in_assets 函数，使用 _APP_MODULE_NAME 替代硬编码的模块名
    - 修改 get_licenses、_get_licenses_html 和 get_about_html 函数，使用 _APP_MODULE_NAME 替代硬编码的模块名

--- vcf_generator/util/resource.py
+++ vcf_generator/util/resource.py
@@ -5,6 +5,8 @@ import pkgutil
 from vcf_generator import constants
 import json
 
+_APP_MODULE_NAME = "vcf_generator"
+
 
 def get_path_in_assets(file_name: str) -> str:
     """
@@ -12,7 +14,7 @@ def get_path_in_assets(file_name: str) -> str:
     :param file_name: The name of the file.
     :return: The path to the file.
     """
-    return os.path.join(os.path.dirname(sys.modules["vcf_generator"].__file__), "assets", file_name)
+    return os.path.join(os.path.dirname(sys.modules[_APP_MODULE_NAME].__file__), "assets", file_name)
 
 
 def get_window_icon() -> Optional[str]:
@@ -28,11 +30,11 @@ def get_default_color() -> str:
 
 
 def get_licenses() -> List[Dict[str, str]]:
-    return json.loads(pkgutil.get_data('vcf_generator', 'assets/data/licenses.json'))
+    return json.loads(pkgutil.get_data(_APP_MODULE_NAME, 'assets/data/licenses.json'))
 
 
 def _get_licenses_html() -> str:
-    projects = json.loads(pkgutil.get_data('vcf_generator', 'assets/data/licenses.json'))
+    projects = json.loads(pkgutil.get_data(_APP_MODULE_NAME, 'assets/data/licenses.json'))
     item_template = '<a href="{url}">{name}</a> - {url}<br />'
     return "".join([
         item_template.format(url=item["url"], name=item["name"]) for item in projects
@@ -40,7 +42,7 @@ def _get_licenses_html() -> str:
 
 
 def get_about_html() -> str:
-    about_html = pkgutil.get_data('vcf_generator', 'assets/texts/about.html').decode('UTF-8', 'ignore')
+    about_html = pkgutil.get_data(_APP_MODULE_NAME, 'assets/texts/about.html').decode('UTF-8', 'ignore')
     about_html = about_html.format(
         source_url=constants.URL_SOURCE,
         release_url=constants.URL_RELEASES,
