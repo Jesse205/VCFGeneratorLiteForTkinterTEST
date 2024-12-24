19c44fa12cd6acdda562cdd3d9f95a291879228d
Jesse205 <2055675594@qq.com>
2024-12-24 14:13:22 +0800

    style(vcf-generator): 优化关于窗口样式
    - 将窗口背景色从白色改为系统默认颜色
    - 调整窗口布局和控件样式，使其更加美观
    - 统一使用像素单位设置控件大小和间距
    - 调整应用名称字体大小，提高可读性

--- vcf_generator/ui/about/__init__.py
+++ vcf_generator/ui/about/__init__.py
@@ -19,7 +19,7 @@ class AboutWindow(BaseToplevel):
         self.resizable(False, False)
 
     def on_init_widgets(self):
-        header_frame = tk.Frame(self, background="white")
+        header_frame = tk.Frame(self, background="systemWindow")
         header_frame.pack(fill=X)
         self.on_init_header(header_frame)
         details_input = HTMLScrolledText(
@@ -28,22 +28,24 @@ class AboutWindow(BaseToplevel):
             html=get_about_html(),
             state=DISABLED,
         )
-        details_input.pack(fill=BOTH, expand=True, **self.scale_values(padx=10, pady=10))
+        details_input.pack(fill=BOTH, expand=True, padx="10p", pady="10p")
         details_context_menu = TextContextMenu(details_input)
         details_context_menu.bind_to_widget()
 
     def on_init_header(self, header_frame: Frame):
+        header_background = header_frame.cget("background")
         self.app_icon_image = PhotoImage(master=self,
                                          data=get_asset_data("images/icon-48.png"))  # 保存到 Window 中防止回收内存
-        app_icon_label = tk.Label(header_frame, image=self.app_icon_image, background="white",
-                                  **self.scale_values(width=48, height=48))
-        app_icon_label.grid(**self.scale_values(row=0, column=0, rowspan=2, **self.scale_values(padx=5, pady=5)))
+        app_icon_label = tk.Label(header_frame, image=self.app_icon_image, background=header_background,
+                                  width="48p", height="48p")
+        app_icon_label.grid(row=0, column=0, rowspan=2, padx="5p", pady="5p")
 
         app_name_font = self.font.copy()
-        app_name_font.config(size=12)
-        app_name_label = tk.Label(header_frame, text=constants.APP_NAME, font=app_name_font, background="white")
+        app_name_font.config(size=16)
+        app_name_label = tk.Label(header_frame, text=constants.APP_NAME, font=app_name_font,
+                                  background=header_background)
         app_name_label.grid(row=0, column=1, sticky=SW)
-        app_version_label = tk.Label(header_frame, text=f"v{__version__}", background="white")
+        app_version_label = tk.Label(header_frame, text=f"v{__version__}", background=header_background)
         app_version_label.grid(row=1, column=1, sticky=NW)
 
 
