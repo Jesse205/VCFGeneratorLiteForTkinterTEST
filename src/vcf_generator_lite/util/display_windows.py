ab4e8f9e1d1a01b3eff4274b17e989e420cf357e
Jesse205 <2055675594@qq.com>
2025-02-12 06:59:26 +0800

    feat(util): 重构高 DPI显示缩放处理
    - 重构 display.py 和 display_windows.py 模块
    - 优化 Windows 平台下的 DPI 检测和设置逻辑
    - 改进错误处理

--- src/vcf_generator_lite/util/display_windows.py
+++ src/vcf_generator_lite/util/display_windows.py
@@ -1,4 +1,11 @@
+"""
+原始代码来源及版权声明:
+https://github.com/TomSchimansky/CustomTkinter/blob/master/customtkinter/windows/widgets/scaling/scaling_tracker.py
+License: MIT (https://github.com/TomSchimansky/CustomTkinter/blob/master/LICENSE)
+"""
+
 import logging
+from contextlib import contextmanager
 from ctypes import windll, pointer, WinError, get_last_error
 from ctypes.wintypes import HWND, DWORD, UINT
 from enum import Enum
@@ -16,6 +23,7 @@ class MonitorDpiType(Enum):
     标识监视器 (dpi) 设置的每英寸点数。
     https://learn.microsoft.com/zh-cn/windows/win32/api/shellscalingapi/ne-shellscalingapi-monitor_dpi_type
     """
+
     MDT_EFFECTIVE_DPI = 0
     MDT_ANGULAR_DPI = 1
     MDT_RAW_DPI = 2
@@ -31,111 +39,109 @@ class ProcessDpiAwareness(Enum):
     请记住，系统缩放的应用程序可能看起来模糊，并且会读取有关监视器的虚拟化数据以保持兼容性。
     https://learn.microsoft.com/zh-cn/windows/win32/api/shellscalingapi/ne-shellscalingapi-process_dpi_awareness
     """
+
     PROCESS_DPI_UNAWARE = 0
     PROCESS_SYSTEM_DPI_AWARE = 1
     PROCESS_PER_MONITOR_DPI_AWARE = 2
 
 
-_process_dpi_aware = False
+@contextmanager
+def get_dc(hwnd: HWND):
+    hdc = windll.user32.GetDC(hwnd)
+    if not hdc:
+        raise WinError(get_last_error())
+    try:
+        yield hdc
+    finally:
+        windll.user32.ReleaseDC(hwnd, hdc)
 
 
-def _set_process_dpi_aware_win8_1():
+def _enable_dpi_aware_win8_1():
     """
     https://learn.microsoft.com/zh-cn/windows/win32/api/shellscalingapi/nf-shellscalingapi-setprocessdpiawareness
     """
-    windll.shcore.SetProcessDpiAwareness(ProcessDpiAwareness.PROCESS_SYSTEM_DPI_AWARE.value)
+    result: int = windll.shcore.SetProcessDpiAwareness(ProcessDpiAwareness.PROCESS_SYSTEM_DPI_AWARE.value)
+    if result != 0:
+        raise WinError(get_last_error())
 
 
-def _set_process_dpi_aware_win_vista():
+def _enable_dpi_aware_vista():
     """
     https://learn.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-setprocessdpiaware
     """
-    windll.user32.SetProcessDPIAware()
+    result = bool(windll.user32.SetProcessDPIAware())
+    if not result:
+        raise WinError(get_last_error())
 
 
-def set_process_dpi_aware():
+def enable_dpi_aware_windows():
     """
     设置进程默认 DPI 感知级别。
 
     该函数尝试将当前进程设置为 DPI 感知模式，以便在高 DPI 显示器上正确缩放。
-    它针对不同的 Windows 版本尝试不同的设置方法。如果在所有尝试均失败或在非 Windows 平台上运行，
-    则该函数不会产生任何影响。
+    它针对不同的 Windows 版本尝试不同的设置方法。
 
     https://learn.microsoft.com/zh-cn/windows/win32/api/shellscalingapi/nf-shellscalingapi-setprocessdpiawareness
     """
-    global _process_dpi_aware
+    last_error = None
 
     for setter in (
-        _set_process_dpi_aware_win8_1,
-        _set_process_dpi_aware_win_vista,
+        _enable_dpi_aware_win8_1,
+        _enable_dpi_aware_vista,
     ):
         try:
             setter()
-        except (AttributeError, OSError) as e:
-            logging.debug(f"Failed to set process DPI awareness",exc_info=e)
-        else:
-            _process_dpi_aware = True
             break
+        except (AttributeError, OSError) as e:
+            last_error = e
+            logging.debug(f"Failed to set process DPI awareness", exc_info=e)
+    else:
+        raise RuntimeError(last_error)
 
 
 def _get_scale_factor_win10(misc: Misc) -> float:
     """
     https://learn.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-getdpiforwindow
     """
-    hwnd = HWND(misc.winfo_id())
-    return windll.user32.GetDpiForWindow(hwnd) / DEFAULT_DPI
+
+    return windll.user32.GetDpiForWindow(HWND(misc.winfo_id())) / DEFAULT_DPI
 
 
 def _get_scale_factor_win8_1(misc: Misc) -> float:
-    shcore = windll.shcore
-    user32 = windll.user32
-    hwnd = HWND(misc.winfo_id())
-    monitor_handle = user32.MonitorFromWindow(hwnd, DWORD(MONITOR_DEFAULTTONEAREST))
+    monitor_handle = windll.user32.MonitorFromWindow(
+        HWND(misc.winfo_id()),
+        DWORD(MONITOR_DEFAULTTONEAREST)
+    )
     dpi_x, dpi_y = UINT(), UINT()
-    shcore.GetDpiForMonitor(monitor_handle, MonitorDpiType.MDT_EFFECTIVE_DPI.value, pointer(dpi_x), pointer(dpi_y))
+    windll.shcore.GetDpiForMonitor(
+        monitor_handle,
+        MonitorDpiType.MDT_EFFECTIVE_DPI.value,
+        pointer(dpi_x),
+        pointer(dpi_y),
+    )
 
     return (dpi_x.value + dpi_y.value) / 2 / DEFAULT_DPI
 
 
-def _get_scale_factor_win2000() -> float:
-    gdi32 = windll.gdi32
-    user32 = windll.user32
-
-    hdc = user32.GetDC(None)
-    if not hdc:
-        raise WinError(get_last_error())
-
-    try:
-        dpi_x = gdi32.GetDeviceCaps(hdc, LOGPIXELSX)
-        dpi_y = gdi32.GetDeviceCaps(hdc, LOGPIXELSY)
-    finally:
-        user32.ReleaseDC(None, hdc)
+def _get_scale_factor_win2000(misc: Misc) -> float:
+    with get_dc(HWND(misc.winfo_id())) as hdc:
+        dpi_x = windll.gdi32.GetDeviceCaps(hdc, LOGPIXELSX)
+        dpi_y = windll.gdi32.GetDeviceCaps(hdc, LOGPIXELSY)
 
     return (dpi_x + dpi_y) / 2 / DEFAULT_DPI
 
 
-def get_scale_factor(misc: Misc) -> float:
-    """
-    获取缩放因子。
-
-    在不同操作系统上获取缩放因子，以便在高 DPI 屏幕上正确显示界面。
-    缩放因子影响如何将逻辑坐标转换为屏幕坐标。
-
-    :param misc: Misc对象
-    :return: 缩放因子，如果无法确定则默认返回 1.0。
-    """
-    if not _process_dpi_aware:
-        return 1.0
-
+def get_scale_factor_windows(misc: Misc) -> float:
+    last_error = None
     for getter in (
-        lambda: _get_scale_factor_win10(misc),
-        lambda: _get_scale_factor_win8_1(misc),
+        _get_scale_factor_win10,
+        _get_scale_factor_win8_1,
         _get_scale_factor_win2000,
     ):
         try:
-            return getter()
+            return getter(misc)
         except (AttributeError, OSError) as e:
+            last_error = e
             logging.debug(f"Failed to get scale factor", exc_info=e)
-
-    # 不支持缩放，默认返回1
-    return 1.0
+    else:
+        raise RuntimeError(last_error)
