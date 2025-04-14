570d7e434f9fca615d8e811e40b10812fcf55fa4
Jesse205 <2055675594@qq.com>
2025-04-15 03:08:16 +0800

    refactor(vcf_generator_lite):重构显示模块初始化逻辑
    - 将 display 对象创建逻辑封装到 create_display 函数中
    - 通过 _display 变量存储创建的 display 对象

--- src/vcf_generator_lite/util/display/__init__.py
+++ src/vcf_generator_lite/util/display/__init__.py
@@ -1,11 +1,16 @@
+from vcf_generator_lite.util.display.base import Display
 from vcf_generator_lite.util.display.null_impl import NullDisplay
 from vcf_generator_lite.util.display.windows_10_impl import Windows10Display
 from vcf_generator_lite.util.environment import is_windows
 
-if is_windows:
-    display = Windows10Display()
-else:
-    display = NullDisplay()
 
-get_default_scale_factor = display.get_default_scale_factor
-enable_dpi_aware = display.enable_dpi_aware
+def create_display() -> Display:
+    if is_windows:
+        return Windows10Display()
+    else:
+        return NullDisplay()
+
+
+_display = create_display()
+get_default_scale_factor = _display.get_default_scale_factor
+enable_dpi_aware = _display.enable_dpi_aware
