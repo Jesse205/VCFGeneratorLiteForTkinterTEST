1fc519d6ea84b6c9782ca71d99816038f714b4c3
Jesse205 <2055675594@qq.com>
2025-04-16 01:35:51 +0800

    fix(vcf_generator_lite): 优化 NullDisplay 类的默认缩放因子方法
    - 将 get_default_scale_factor 方法的返回值从 pass 修改为返回 1.0
    - 这个改动使得 NullDisplay 类在需要默认缩放因子时能够提供一个合理的默认值

--- src/vcf_generator_lite/util/display/null_impl.py
+++ src/vcf_generator_lite/util/display/null_impl.py
@@ -5,7 +5,7 @@ from vcf_generator_lite.util.display.base import Display
 
 class NullDisplay(Display):
     def get_default_scale_factor(self, misc: Misc) -> float:
-        pass
+        return 1.0
 
     def enable_dpi_aware(self):
         pass
