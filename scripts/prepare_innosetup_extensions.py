864556eccc9c61a73f207473b4df28dbaadcf3ed
Jesse205 <2055675594@qq.com>
2025-05-30 04:31:06 +0800

    refactor(scripts): 移除字符串替换
    新字符串已合并到 kira-96/Inno-Setup-Chinese-Simplified-Translation

--- scripts/prepare_innosetup_extensions.py
+++ scripts/prepare_innosetup_extensions.py
@@ -1,20 +1,16 @@
 import argparse
 import os
-import re
 import sys
 
 import requests
 
 URL_CHINESE_SIMPLIFIED_ISL_URL = "https://raw.github.com/jrsoftware/issrc/main/Files/Languages/Unofficial/ChineseSimplified.isl"
+URL_CHINESE_SIMPLIFIED_ISL_LATEST = "https://github.com/kira-96/Inno-Setup-Chinese-Simplified-Translation/raw/refs/heads/main/ChineseSimplified.isl"
 # 国内使用 GitCode 加速下载
 URL_CHINESE_SIMPLIFIED_ISL_GITCODE = "https://raw.gitcode.com/gh_mirrors/is/issrc/raw/main/Files/Languages/Unofficial/ChineseSimplified.isl"
 
 PATH_INNOSETUP_EXTENSION = "./.innosetup"
 PATH_CHINESE_SIMPLIFIED = os.path.join(PATH_INNOSETUP_EXTENSION, "Languages", "ChineseSimplified.isl")
-
-TEXT_REPLACE_DICT_CHINESE_SIMPLIFIED = {
-    "PrivilegesRequiredOverrideText2": "仅为您安装 %1，或为所有用户安装(需要管理员权限)。"
-}
 
 
 def prepare_innosetup_extensions(download_url: str) -> int:
@@ -26,8 +22,7 @@ def prepare_innosetup_extensions(download_url: str) -> int:
     file_text = response.text
     # 获取到的内容是CRLF换行的，但是python只能识别LF换行，所以需要替换一下
     file_text = file_text.replace("\r", "")
-    for key, value in TEXT_REPLACE_DICT_CHINESE_SIMPLIFIED.items():
-        file_text = re.sub(f"^{key}=.*$", f"{key}={value}", file_text, flags=re.MULTILINE)
+
     os.makedirs(os.path.dirname(PATH_CHINESE_SIMPLIFIED), exist_ok=True)
     with open(PATH_CHINESE_SIMPLIFIED, "wt", encoding=response.encoding, newline="\r\n") as f:
         f.write(file_text)
@@ -40,14 +35,16 @@ def main() -> int:
     parser.add_argument(
         "-m", "--mirror",
         type=str,
-        default="github",
-        choices=["github", "gitcode"],
+        default="latest",
+        choices=["github", "gitcode", "latest"],
         help="文件下载镜像（默认：%(default)s）"
     )
     args = parser.parse_args()
     match args.mirror:
         case "github":
             download_url = URL_CHINESE_SIMPLIFIED_ISL_URL
+        case "latest":
+            download_url = URL_CHINESE_SIMPLIFIED_ISL_LATEST
         case "gitcode":
             download_url = URL_CHINESE_SIMPLIFIED_ISL_GITCODE
         case _:
