f8a2e706ef6bfe5b74e4b70852282aa2ff58514b
Jesse205 <2055675594@qq.com>
2026-02-14 22:41:45 +0800

    ```refactor(vcf_generator_lite): 移除未使用的关闭加速键常量并重新组织代码结构
    移除了被注释掉的 ACCELERATOR_CLOSE 常量定义，并将相关注释移动到适当位置，
    使代码更加清晰和简洁。同时调整了常量的排列顺序以提高可读性。

--- src/vcf_generator_lite/utils/tkinter/accelerators.py
+++ src/vcf_generator_lite/utils/tkinter/accelerators.py
@@ -1,9 +1,6 @@
-# ACCELERATOR_CLOSE = "Alt+F4"
-
-# Edit accelerators
 from tkinter import Misc
 
-
+# Edit accelerators
 ACCELERATOR_UNDO = "Ctrl+Z"
 ACCELERATOR_REDO_WINDOWS = "Ctrl+Y"
 ACCELERATOR_REDO_X11 = "Ctrl+Shft+Z"
@@ -11,6 +8,7 @@ ACCELERATOR_CUT = "Ctrl+X"
 ACCELERATOR_COPY = "Ctrl+C"
 ACCELERATOR_PASTE = "Ctrl+V"
 ACCELERATOR_SELECT_ALL = "Ctrl+A"
+# ACCELERATOR_CLOSE = "Alt+F4"
 
 
 def get_accelerator_redo(master: Misc) -> str:
