658a618b97ee722ff0119879e15add1fc8eebabc
Jesse205 <2055675594@qq.com>
2026-03-16 07:25:19 +0800

    refactor(constants): 重构URL常量定义，修复反馈链接错误
    移除硬编码的仓库地址，改用URL_REPOSITORY常量进行拼接，
    提高代码维护性和一致性

--- src/vcf_generator_lite/constants.py
+++ src/vcf_generator_lite/constants.py
@@ -4,12 +4,10 @@ APP_COPYRIGHT = "Copyright © 2023-2026 Jesse205"
 
 SOURCE_REFERENCE = "master" if __debug__ else "v" + __version__
 
-URL_RELEASES = "https://gitee.com/hellotool/VCFGeneratorLiteWithTkinter/releases"
 URL_REPOSITORY = "https://gitee.com/hellotool/VCFGeneratorLiteWithTkinter"
-URL_REPORT = "https://gitee.com/HelloThellotoolool/VCFGeneratorLiteWithTkinter/issues/new/choose"
-URL_LICENSE = f"https://gitee.com/hellotool/VCFGeneratorLiteWithTkinter/blob/{SOURCE_REFERENCE}/LICENSE"
-URL_OS_NOTICES = (
-    f"https://gitee.com/hellotool/VCFGeneratorLiteWithTkinter/blob/{SOURCE_REFERENCE}/docs/legal/os-notices.md"
-)
+URL_RELEASES = f"{URL_REPOSITORY}/releases"
+URL_REPORT = f"{URL_REPOSITORY}/issues/new/choose"
+URL_LICENSE = f"{URL_REPOSITORY}/blob/{SOURCE_REFERENCE}/LICENSE"
+URL_OS_NOTICES = f"{URL_REPOSITORY}/blob/{SOURCE_REFERENCE}/docs/legal/os-notices.md"
 
 EMAIL_JESSE205 = "jesse205@qq.com"
